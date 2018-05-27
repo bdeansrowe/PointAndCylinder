d795f96691b8336749b446b028dd0510aae24f5a
Brian Deans-Rowe <bdeansrowe@gmail.com>
2018-05-27 17:17:55 -0400

    one and done (hopefully)

--- README.md
+++ README.md
@@ -1,25 +1,69 @@
-# ReduxSimpleStarter
+# PointAndCylinder
 
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
 
-### Getting Started
+## Problem statement
 
-There are two methods for getting started with this repo.
+Problem statement: Using Javascript, please create a function that, given any point and a hollow, closed cylinder, determines the distance from the point to the surface of the cylinder, and a simple GUI to exercise this function. 
 
-#### Familiar with Git?
-Checkout this repo, install dependencies, then start the gulp process with the following:
+You will need to determine how to represent points and cylinders. You can assume Cartesian coordinates and Euclidean geometry. 
+
+## Solution Discusion
+
+### Note on the algorithm
+
+This algorithm is a modification of the distance computation between a point and a cylinder from Barbier & Galin's [Fast Distance Computation Between a Point and Cylinders, Cones, Line Swept Spheres and Cone-Spheres](http://liris.cnrs.fr/Documents/Liris-1297.pdf).
+
+The modifications from the paper are to compute the closest distance when the point is interior to the capped cylinder.
+
+### Inputs
+
+The cylinder's axis is defined by a pair of points: __a__ and __b__.
+
+The cylinder's radius is defined as the number _r_.
+
+We are looking for the shortest distance from a point __p__ to the surface of the closed hollow cylinder.
+
+### Intermediates
+
+The point __c__, is the center point of the line segment [__a__, __b__] and is the center point of the cylinder's axis.
+
+The length of the axis, (or the height of the cylinder), is: _l_ = &#x2016;__b__ - __a__&#x2016;.
+
+We define the unit axis vector, a unit vector in the direction of the axis vector, as: __u__ = (__b__ - __a__)/ &#x2016;__b__ - __a__&#x2016;.
+
+We use the unit axis vector, __u__, to compute the scalar projection of the vector from __c__ to __p__ onto the axis, which is a signed distance along the axis from __c__: _x_ = (__p__ - __c__)&#x2219;__u__.
+
+We further define the coordinate of __p__ orthogonally projected onto the axis as __h__.
+
+The squared distance of __p__ from __c__ is: _n_&#xb2; = &#x2016;__p__ - __c__&#x2016;&#xb2;.
+
+Finally the squared distance from __p__ to the axis is: _y_&#xb2; = &#x2016;__p__ - __h__&#x2016;&#xb2; = _n_&#xb2; - _x_&#xb2;.
+
+### Distance to Cylinder
+
+The points __a__, __b__, and __p__ form a plane within which, because of the symmetry of the cylinder, the problem simplifies down to a straight forward two-dimensional analysis.
+
+We use these computed intermediaries as metrics to seperate the possible arrangements of __p__ and the cylinder and compute the distance to its surface.
+
+The first case is if |_x_| <= (_l_ / 2), and _y_&#xb2; > _r_&#xb2;.
+
+In this case, __h__ is on the line segment [__a__, __b__], on the axis, and __p__ is exterior to the cylinder. The distance to the cylinder is simply: _d_ = _y_ - _r_.
+
+The second case is if |_x_| > (_l_ / 2), which means that __h__ is beyond the ends of [__a__, __b__], and breaks down into two sub-cases.
+
+First, if _y_&#xb2; > _r_&#xb2;, then the distance to the cylinder is the distance to the circle defining the end of the cylinder: _d_ = __SQRT(__(_y_ - _r_)&#xb2; + (|_x_| - (_l_ / 2))&#xb2;__)__.
+
+Second, if _y_&#xb2; < _r_&#xb2;, then the distance to the cylinder is the distance to the cylinder's endcap: _d_ = (|_x_| - _l_/2).
+
+The final case exist if |_x_| < _l_/2, and _y_&#xb2; < _r_&#xb2;. In this case __p__ is interior to the cylinder, and the result is differentiated by whether __p__ is closer to the cylinder's sides, or closer to the cylinder's endcaps. In this case we compute both possibilities and take the minimum: _d_ = __MIN(__ |(_y_ - _r_)|__,__ |(_l_/2 - |_x_|)| __)__.
+
+##  Git Installation and Execution
+Checkout this repo, install dependencies, then start the webpack-dev-server process with the following:
 
 ```
-> git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
-> cd ReduxSimpleStarter
+> git clone https://github.com/bdeansrowe/PointAndCylinder.git
+> cd PointAndCylinder
 > npm install
 > npm start
 ```
-
-#### Not Familiar with Git?
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
-
-```
-> npm install
-> npm start
-```
+Then open a browser window on 'localhost:9090'
--- src/index.js
+++ src/index.js
@@ -1,15 +1,258 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import NumericInput from 'react-numeric-input';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+var mag = require('vectors/mag')(3);
+var sub = require('vectors/sub')(3, {scalers: false});
+var copy = require('vectors/copy')(3);
+var mult = require('vectors/mult')(3, { vectors: false });
+var add = require('vectors/add')(3);
+var normalize = require('vectors/normalize')(3)
+var dot = require('vectors/dot')(3)
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+import Vector3 from './components/vector3';
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			a: [-5.0, 0.0, 0.0],
+			b: [5.0, 0.0, 0.0],
+			r: 2.0,
+			p: [3.0, 3.0, 0.0],
+			l: 0.0,
+			c: [0.0, 0.0, 0.0],
+			u: [0.0, 0.0, 0.0],
+			x: 19.0,
+			h: [20.0, 21.0, 22.0],
+			n2: 23.0,
+			y2: 24.0,
+			d: 25.0,
+			solution_case: ''
+		};
+
+		this.onVectorComponentChange = this.onVectorComponentChange.bind(this);
+	}
+
+	componentDidMount() {
+		this.compute();
+	}
+
+	compute() {
+		this.length();
+		this.center();
+		this.unitAxisVector();
+		// all other computes are dependent on these and called
+		// in the cb to setState of the state value they are dependent on		
+		this.forceUpdate();
+	}
+
+	length() {
+		let l = copy(this.state.a);
+		sub(l, this.state.b);
+
+		this.setState({l: mag(l)}, () => {this.distanceToCylinder()});
+	}
+
+	center() {
+		let c = copy(this.state.a);
+		let ab = copy(this.state.b);
+		sub(ab, c);
+		mult(ab, 0.5);
+		add(c, ab);
+
+		this.setState({c: c}, () => {this.ex(); this.enSquared()});
+	}
+
+	unitAxisVector() {
+		let u = copy(this.state.b);
+		let a = copy(this.state.a);
+
+		sub(u, a);
+		normalize(u);
+
+		this.setState({u: u}, () => this.ex());
+}
+
+	ex() {
+		let c = copy(this.state.c);
+		let p = copy(this.state.p);
+		let u = copy(this.state.u);
+
+		sub(p, c);
+
+		let x = dot(p, u);
+
+		this.setState({x: x}, () => {this.aitch(); this.ySquared()});
+	}
+
+	aitch() {
+		let h = copy(this.state.c);
+		let u = copy(this.state.u);
+		let x = this.state.x;
+
+		add(h, mult(u, x));
+
+		this.setState({h: h});
+	}
+
+	enSquared() {
+		let p = copy(this.state.p);
+		let c = copy(this.state.c);
+
+		sub(p, c);
+
+		let n2 = mag(p)**2;
+
+		this.setState({n2: n2}, () => this.ySquared());
+	}
+
+	ySquared() {
+		let n2 = this.state.n2;
+		let x2 = this.state.x**2;
+
+		let y2 = n2 - x2;
+
+		this.setState({y2: y2}, () => {this.distanceToCylinder()});
+	}
+
+	distanceToCylinder() {
+		let absX = Math.abs(this.state.x);
+		let l = this.state.l;
+		let half_l = l / 2.0;
+		let y2 = this.state.y2;
+		let y = y2**0.5;
+		let r = this.state.r;
+		let r2 = r**2;
+
+		let d = 0.0;
+		let solution_case = '';
+
+		if (absX <= half_l) {
+			if (y2 >= r2) {
+				d = y - r;
+				solution_case = 'Ia: P is exterior to the cylinder and within the bounds of the axis';
+			} else {
+				d = Math.min(Math.abs(y - r), Math.abs(half_l - absX));
+				solution_case = 'Ib: P is interior to the cylinder and either closer to an end-cap or the cylinder wall';
+			}
+		} else {
+			if (y2 >= r2) {
+				d = (((y-r)**2) + (absX - half_l)**2)**0.5;
+				solution_case = "IIa: P is beyond the bounds of the cylinder's axis and radius";
+			} else {
+				d = absX - half_l;
+				solution_case = "IIb: P is beyond the bounds of the cylinder's axis but within the bounds of it's radius";
+			}
+		}
+
+		this.setState({d: d, solution_case: solution_case});
+	}
+
+	onVectorComponentChange(valueAsNumber, vector_name, component_index) {
+		let v3 = this.state[vector_name];
+
+		v3[component_index] = valueAsNumber;
+		let new_state = {};
+		new_state[vector_name] = v3;
+		this.setState(new_state);
+		this.compute();
+	}
+
+	render() {
+		return (
+			<div>
+				<h1> Inputs </h1>
+				<div>
+					<p> Cylinder defined by axis points and radius </p>
+					A &nbsp;[<Vector3
+						v3={this.state.a}
+						vector_name='a'
+						onChange={this.onVectorComponentChange} />]
+					&nbsp;
+					B &nbsp;[<Vector3
+						v3={this.state.b}
+						vector_name='b'
+						onChange={this.onVectorComponentChange} />]
+					&nbsp;
+					Radius &nbsp;
+						<NumericInput
+							step={0.1}
+							precision={3}
+							min={0}
+							size={5}
+							value={this.state.r}
+							onChange={valueAsNumber => {
+								this.setState({r: valueAsNumber}, this.compute());
+							}} />
+				</div>
+				<div>
+					<br/>
+					<p> Point to find distance to cylinder </p>
+					P &nbsp;[<Vector3
+						v3={this.state.p}
+						vector_name='p'
+						onChange={this.onVectorComponentChange} />]
+				</div>
+				<div><hr/></div>
+				<div>
+					<h1> Computed Intermediates </h1>
+					<p> l: Cylinder Length &nbsp;
+						<NumericInput
+							precision={3}
+							size={6}
+							readOnly
+							value={this.state.l} />
+					</p>
+					<p> C: Axis Center &nbsp;[<Vector3 v3={this.state.c} readOnly />] </p>
+					<p> u: Unit Axis Vector &nbsp;[<Vector3 v3={this.state.u} readOnly />] </p>
+					<p> x: Scaler projection of vector PC on the axis &nbsp;
+						<NumericInput
+							precision={3}
+							size={6}
+							readOnly
+							value={this.state.x} />
+					</p>
+					<p> h: Coordinate of P orthogonally projected onto the axis &nbsp;
+						[<Vector3 v3={this.state.h} readOnly />]
+					</p>
+					<p> n2: Squared distance from P to C &nbsp;
+						<NumericInput
+							precision={3}
+							size={6}
+							readOnly
+							value={this.state.n2} />
+					</p>
+					<p> y2: Squared distance from P to the axis &nbsp;
+						<NumericInput
+							precision={3}
+							size={6}
+							readOnly
+							value={this.state.y2} />
+					</p>
+				</div>
+				<div><hr/></div>
+				<div>
+					<h1> Distance from P to Cylinder </h1>
+					<p> d: &nbsp;
+						<NumericInput
+							precision={3}
+							size={6}
+							readOnly
+							value={this.state.d} /> &nbsp;
+						Solution case {this.state.solution_case}
+					</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+// Take this component's generated HTML and put it'
+// on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
